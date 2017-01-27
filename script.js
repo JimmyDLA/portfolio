3dfae7d46c1faf9177c31bf4ceed302d37fa80f1
Jimmy De Los Angeles <Jimmy De Los Angeles>
2017-01-26 19:44:51 -0500

    logo and contact done

--- script.js
+++ script.js
@@ -1,98 +1,97 @@
 
+function randomColor(){
+  let rgb1 = Math.floor(Math.random() * 255);
+  let rgb2 = Math.floor(Math.random() * 255);
+  let rgb3 = Math.floor(Math.random() * 255);
+  let rgba =  "rgba(" + rgb1 + "," + rgb2 + "," + rgb3 + "," + "0.1)";
+  return rgba;
+}
 
-  function randomColor(){
-    let rgb1 = Math.floor(Math.random() * 255);
-    let rgb2 = Math.floor(Math.random() * 255);
-    let rgb3 = Math.floor(Math.random() * 255);
-    let rgba =  "rgba(" + rgb1 + "," + rgb2 + "," + rgb3 + "," + "0.1)";
-    return rgba;
+function flashingLights(){
+  let temp = Math.floor(Math.random()*3);
+  if(temp === 0){
+    return "rgba(255,165,0,0.1)";
+  }if(temp === 1){
+    return "rgba(200,200,200,0.1)";
+  }if(temp === 2){
+    return "rgba(0,125,255,0.1)"
   }
+}
 
-  function flashingLights(){
-    let temp = Math.floor(Math.random()*3);
-    if(temp === 0){
-      return "rgba(255,165,0,0.1)";
-    }if(temp === 1){
-      return "rgba(200,200,200,0.1)";
-    }if(temp === 2){
-      return "rgba(0,125,255,0.1)"
-    }
+function widHeight(){
+  num = Math.floor(Math.random()* 400);
+  if(num < 150){
+    num = num + 200;
   }
+  return num;
+}
+function yAxis(){
+  posY = Math.floor(Math.random()* 600);
+  return posY;
+}
+function xAxis(){
+  posX = Math.floor(Math.random()* 1150);
+  return posX
+}
 
-  function widHeight(){
-    num = Math.floor(Math.random()* 400);
-    if(num < 150){
-      num = num + 200;
-    }
-    return num;
-  }
-  function yAxis(){
-    posY = Math.floor(Math.random()* 600);
-    return posY;
-  }
-  function xAxis(){
-    posX = Math.floor(Math.random()* 1150);
-    return posX
-  }
+function opacityWhole(){
+  this.style.opacity = "1";
+}
+function opacityMinus(){
+  this.style.transitionDelay = "0s";
+  this.style.opacity = "0";
+}
 
-  function opacityWhole(){
-    this.style.opacity = "1";
+function makeBub(howMany){
+  //create emement container here
+  //for create bubbles
+  for(let i = 0; i < howMany; i++) {
+    let ranNum = widHeight();
+    let newBubble = document.createElement("DIV")
+    newBubble.className="focus focusFade";
+    newBubble.id = "bubble" + i;
+    newBubble.style.backgroundColor = flashingLights();
+    newBubble.style.height = ranNum + "px";
+    newBubble.style.width = ranNum + "px";
+    newBubble.style.top = yAxis() + "px";
+    newBubble.style.left = xAxis() + "px";
+    newBubble.style.transitionDelay = `${i*0.1}s`
+    newBubble.addEventListener("mouseout", opacityWhole);
+    newBubble.addEventListener("mouseover", opacityMinus);
+    document.querySelector("#main").appendChild(newBubble);
+    newBubble.focus();
+    newBubble.className = "focus";
   }
-  function opacityMinus(){
-    this.style.transitionDelay = "0s";
-    this.style.opacity = "0";
-  }
+}
 
-  function makeBub(howMany){
-    //create emement container here
-    //for create bubbles
-    for(let i = 0; i < howMany; i++) {
-      let ranNum = widHeight();
-      let newBubble = document.createElement("DIV")
-      newBubble.className="focus focusFade";
-      newBubble.id = "bubble" + i;
-      newBubble.style.backgroundColor = flashingLights();
-      newBubble.style.height = ranNum + "px";
-      newBubble.style.width = ranNum + "px";
-      newBubble.style.top = yAxis() + "px";
-      newBubble.style.left = xAxis() + "px";
-      newBubble.style.transitionDelay = `${i*0.1}s`
-      newBubble.addEventListener("mouseout", opacityWhole);
-      newBubble.addEventListener("mouseover", opacityMinus);
-      document.querySelector("#main").appendChild(newBubble);
-      newBubble.focus();
-      newBubble.className = "focus";
-    }
-  }
-
-  function makeJ(){
-    let myName = document.querySelector("#letterJ");
-    myName.style.opacity = "1";
-  }
-  function makeI(){
-    let myName = document.querySelector("#letterI");
-    myName.style.opacity = "1";
-  }
-  function makeM(){
-    let myName = document.querySelector("#letterM");
-    myName.style.opacity = "1";
-  }
-  function makeMm(){
-    let myName = document.querySelector("#letterMm");
-    myName.style.opacity = "1";
-  }
-  function makeY(){
-    let myName = document.querySelector("#letterY");
-    myName.style.opacity = "1";
-  }
-  function makeLname(){
-    let myName = document.querySelector(".lName");
-    myName.style.opacity = "1";
-  }
-  function makeWebDev(){
-    let myName = document.querySelector(".webDev");
-    myName.style.opacity = "1";
-  }
+function makeJ(){
+  let myName = document.querySelector("#letterJ");
+  myName.style.opacity = "1";
+}
+function makeI(){
+  let myName = document.querySelector("#letterI");
+  myName.style.opacity = "1";
+}
+function makeM(){
+  let myName = document.querySelector("#letterM");
+  myName.style.opacity = "1";
+}
+function makeMm(){
+  let myName = document.querySelector("#letterMm");
+  myName.style.opacity = "1";
+}
+function makeY(){
+  let myName = document.querySelector("#letterY");
+  myName.style.opacity = "1";
+}
+function makeLname(){
+  let myName = document.querySelector(".lName");
+  myName.style.opacity = "1";
+}
+function makeWebDev(){
+  let myName = document.querySelector(".webDev");
+  myName.style.opacity = "1";
+}
 
   // THIS IS TO MAKE HOW MANY BUBBLES
 makeBub(40)
